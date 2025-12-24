9d69df4e03127b751cd9e9e49406a1a0f235ec97
Suhaylah <suhailahawsawi@gmail.com>
2025-12-24 18:53:38 +0300

    movies overview

---
+++ Movieisme/Views/MovieDetails.swift
@@ -0,0 +1,101 @@
+//
+//  MovieDetails.swift
+//  Movieisme
+//
+//  Created by Suhaylah hawsawi on 04/07/1447 AH.
+//
+
+import SwiftUI
+
+struct MovieDetails: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ZStack {
+                        Image("Shawshankscoverimage")
+                            .resizable()
+                            .scaledToFill()
+                            .padding(.top, -80)
+                        
+
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                Color.black.opacity(0.9),
+                                Color.black.opacity(0)
+                            ]),
+                            startPoint: .bottom,
+                            endPoint: .top
+                        )
+                    }
+                    
+                    Text("Shawshank")
+                        .font(.system(size: 28, weight: .bold))
+                      
+                    
+                    Spacer().frame(height: 40)
+
+                    MoivesOverview()
+                        
+                    }
+                }
+            }
+        }
+
+    }
+
+
+struct MoivesOverview: View {
+    var body: some View {
+        
+        LazyVGrid(columns: [
+            GridItem(.fixed(119),spacing: 70),
+            GridItem(.fixed(100),spacing: 70)
+        ],alignment: .leading,  spacing: 32){
+            
+            VStack(alignment: .leading, spacing: 8){
+                Text("Duration")
+                    .font(.system(size: 18, weight: .semibold))
+                
+                Text("2 hours 22 mins")
+                    .font(.system(size: 15, weight: .medium))
+                    .foregroundColor(Color("greyish"))
+            }
+            
+            
+            VStack(alignment: .leading, spacing: 8){
+                Text("Language")
+                    .font(.system(size: 18, weight: .semibold))
+                
+                Text("English")
+                    .font(.system(size: 15, weight: .medium))
+                    .foregroundColor(Color("greyish"))
+                
+            }
+            
+            VStack(alignment: .leading, spacing: 8){
+                Text("Genre")
+                    .font(.system(size: 18, weight: .semibold))
+                
+                Text("Drama")
+                    .font(.system(size: 15, weight: .medium))
+                    .foregroundColor(Color("greyish"))
+                
+            }
+            
+            VStack(alignment: .leading, spacing: 8){
+                Text("Age")
+                    .font(.system(size: 18, weight: .semibold))
+                
+                Text("+15")
+                    .font(.system(size: 15, weight: .medium))
+                    .foregroundColor(Color("greyish"))
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MovieDetails()
+}
