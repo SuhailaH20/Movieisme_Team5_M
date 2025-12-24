14f19fdcf8bc81a50520b032e1729011edfe2e64
Suhaylah <suhailahawsawi@gmail.com>
2025-12-24 09:38:24 +0300

    first commit

---
+++ Movieisme/Views/Signin.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Movieisme
+//
+//  Created by Suhaylah hawsawi on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+struct SigninPage: View {
+    var body: some View {
+      
+            Image("Signinbackground")
+            .resizable()
+            .frame(width: 400, height: 694)
+            .ignoresSafeArea()
+            Spacer()
+    
+
+      
+    }
+}
+
+#Preview {
+    SigninPage()
+}
