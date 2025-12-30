cb30b944375a85cc68a6371a91d30865df6587b4
Suhaylah <suhailahawsawi@gmail.com>
2025-12-30 15:16:49 +0300

    user authentication added

---
+++ Movieisme/ViewModel/AuthViewModel.swift
@@ -0,0 +1,43 @@
+@MainActor
+class AuthViewModel: ObservableObject {
+
+    @Published var email = ""
+    @Published var password = ""
+
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    @Published var currentUser: User?
+
+    func signIn() async {
+        guard !email.isEmpty, !password.isEmpty else { return }
+
+        isLoading = true
+        errorMessage = nil
+
+        do {
+            let url = URL(
+                string: "https://api.airtable.com/v0/appsfcB6YESLj4NCN/users"
+            )!
+
+            let data = try await APIClient.fetch(url)
+            let decoded = try JSONDecoder().decode(UsersResponse.self, from: data)
+
+            if let user = decoded.records
+                .map({ $0.fields })
+                .first(where: {
+                    $0.email.lowercased() == email.lowercased()
+                    && $0.password == password
+                }) {
+
+                currentUser = user
+            } else {
+                errorMessage = "Invalid email or password"
+            }
+
+        } catch {
+            errorMessage = "Failed to login"
+        }
+
+        isLoading = false
+    }
+}
